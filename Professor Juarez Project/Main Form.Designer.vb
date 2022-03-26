f03ed061e336404e3f390286400d0a4fba711372
Edward Knueppel <edward.knueppel@zywave.com>
2022-03-26 06:23:09 -0500

    first commit

---
+++ Professor Juarez Project/Main Form.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstGrades = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.lstNames = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(305, 122)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 29)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "&Grade:"
+        '
+        'lstGrades
+        '
+        Me.lstGrades.FormattingEnabled = True
+        Me.lstGrades.ItemHeight = 15
+        Me.lstGrades.Items.AddRange(New Object() {"A", "B", "C", "D", "F"})
+        Me.lstGrades.Location = New System.Drawing.Point(37, 48)
+        Me.lstGrades.Name = "lstGrades"
+        Me.lstGrades.Size = New System.Drawing.Size(52, 94)
+        Me.lstGrades.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(111, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "&Students:"
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(224, 122)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(75, 29)
+        Me.btnDisplay.TabIndex = 0
+        Me.btnDisplay.Text = "&Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(221, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(116, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Number of students:"
+        '
+        'lblNumber
+        '
+        Me.lblNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNumber.Location = New System.Drawing.Point(224, 45)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(60, 23)
+        Me.lblNumber.TabIndex = 7
+        Me.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lstNames
+        '
+        Me.lstNames.FormattingEnabled = True
+        Me.lstNames.ItemHeight = 15
+        Me.lstNames.Location = New System.Drawing.Point(115, 48)
+        Me.lstNames.Name = "lstNames"
+        Me.lstNames.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.lstNames.Size = New System.Drawing.Size(77, 79)
+        Me.lstNames.Sorted = True
+        Me.lstNames.TabIndex = 5
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(528, 24)
+        Me.MenuStrip1.TabIndex = 8
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(37, 20)
+        Me.ToolStripMenuItem1.Text = "File"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.F4), System.Windows.Forms.Keys)
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(135, 22)
+        Me.ToolStripMenuItem2.Text = "Exit"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(528, 229)
+        Me.Controls.Add(Me.lstNames)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lstGrades)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Professor Juarez"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstGrades As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblNumber As Label
+    Friend WithEvents lstNames As ListBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+End Class
---
+++ Professor Juarez Project/Main Form.vb
@@ -0,0 +1,55 @@
+﻿' Name:         Professor Juarez Project
+' Purpose:      Display the names and number of students earning a specific grade.
+' Programmer:   <your name> on <current date>
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click, ToolStripMenuItem2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        ' Display the names and number of students earning a specific grade.
+
+        Dim strNames() As String = {"Helen", "Peter", "Yolanda", "Carl", "Jennifer", "Charles", "Addison", "Aiden", "Treyson", "Sydney", "Jacob", "Nancy", "George", "Ursula", "Jack"}
+        Dim strGrades() As String = {"A", "B", "B", "A", "D", "F", "A", "B", "A", "B", "F", "C", "C", "B", "D"}
+
+        Dim count As Integer = 0
+        Dim strResult(strNames.Length) As String
+        For counter As Integer = 0 To strNames.Length - 1
+            If strGrades(counter).Equals(lstGrades.SelectedItem.ToString) Then
+                count += 1
+                strResult(count - 1) = strNames(counter)
+
+            End If
+
+        Next
+
+        lstNames.SelectionMode = SelectionMode.One
+        lstNames.DataSource = strResult
+        lstNames.Refresh()
+
+        lblNumber.Text = count.ToString
+
+    End Sub
+
+    Private Sub lstGrades_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstGrades.SelectedIndexChanged
+        lstNames.DataSource = Nothing
+        For i As Integer = 0 To lstNames.Items.Count - 1
+            lstNames.Items.RemoveAt(0)
+        Next
+        lstNames.Refresh()
+
+        lblNumber.Text = ""
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstGrades.SelectedIndex = 0
+
+    End Sub
+
+End Class
